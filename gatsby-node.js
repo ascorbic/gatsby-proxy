5a0a710480e287659918fb1e65c95861cb1af698
Matt Kane <matt@gatsbyjs.com>
2022-02-14 11:11:59 +0000

    Update target site

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,13 @@
 exports.createPages = async ({ actions: { createRedirect } }) => {
   createRedirect({
     fromPath: '/reverse-proxy-basepath',
-    toPath: 'https://nextjs-dynamic-path.netlify.app/reverse-proxy-basepath/',
+    toPath: 'https://nf-test-next-proxy-target.netlify.app/reverse-proxy-basepath/',
     statusCode: 200,
   });
 
   createRedirect({
     fromPath: '/reverse-proxy-basepath/*',
-    toPath: 'https://nextjs-dynamic-path.netlify.app/reverse-proxy-basepath/:splat',
+    toPath: 'https://nf-test-next-proxy-target.netlify.app/reverse-proxy-basepath/:splat',
     statusCode: 200,
   });
 };
